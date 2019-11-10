8adb7e713f213c9a01f9a08c0719565c1a2b1eff
Bruno Falis <brunofalis0@gmail.com>
2019-11-10 12:24:11 -0300

    segundo commit

---
+++ src/main/java/com/cvc/teste/thymeleaf/ThymeleafConfig.java
@@ -0,0 +1,31 @@
+package com.cvc.teste.thymeleaf;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.thymeleafexamples")
+public class ThymeleafConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+  private ApplicationContext applicationContext;
+
+  public void setApplicationContext(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  @Bean
+  public ViewResolver viewResolver() {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
+  }
+
+}
